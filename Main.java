843d036cc0a61b255ec747a4870c96d5c20ed913
Akash Panchani <akash61p@gmail.com>
2025-03-25 22:44:58 +0530

    Initial Commit

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main{
         System.out.println("Hello World!");
         System.out.println(new Date());
         System.out.println("Akash Code");
+        System.out.println("Dude");
     }
 }
